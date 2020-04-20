fd669e04c6c113747ee593acc411e01cf32a40e4
Z <mohistzh@gmail.com>
2020-04-20 10:40:30 +0800

    fsp: detecting if linkedlist has a cycle

--- fastslowpointers/linkedlist_cycle.go
+++ fastslowpointers/linkedlist_cycle.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	"github.com/mohistzh/algo/linkedlist"
+	"fmt"
 )
 
-func hasCycle(head *linkedlist.Node) bool {
+//node linked list node
+type node struct {
+	value int
+	next  *node
+}
+
+// hasCycle super easy question, fast and slow pointers pattern
+func hasCycle(head *node) bool {
+	slow := head
+	fast := head.next
+	for fast != nil && fast.next != nil {
+		if slow.value == fast.value {
+			return true
+		}
+		slow = slow.next
+		fast = fast.next.next
+	}
 	return false
 }
 
 func main() {
-	var singlyLinkedList linkedlist.SinglyLinkedList
-	singlyLinkedList.Append(1)
-	singlyLinkedList.Append(2)
-	singlyLinkedList.Append(3)
-	singlyLinkedList.Append(4)
-	singlyLinkedList.Append(5)
-	singlyLinkedList.Append(6)
-	singlyLinkedList.String()
-	singlyLinkedList.Append(4)
-	singlyLinkedList.String()
-
+	head := node{value: 1}
+	head.next = &node{value: 2}
+	head.next.next = &node{value: 3}
+	head.next.next.next = &node{value: 4}
+	head.next.next.next.next = &node{value: 5}
+	head.next.next.next.next.next = &node{value: 6}
+	fmt.Println("LinkedList has cycle:", hasCycle(&head))
+	head.next.next.next.next.next.next = head.next.next
+	fmt.Println("LinkedList has cycle:", hasCycle(&head))
+	head.next.next.next.next.next.next = head.next.next.next
+	fmt.Println("LinkedList has cycle:", hasCycle(&head))
 }
