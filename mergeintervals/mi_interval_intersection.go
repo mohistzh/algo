ebde8982bfbdeae6de1787e35ba0e37f688083c5
Z <mohistzh@gmail.com>
2020-04-26 17:30:40 +0800

    mi: interval intersection problem

---
+++ mergeintervals/mi_interval_intersection.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+	Given two lists of intervals, find the intersection of these two lists.
+	Each list consists of disjoint intervals sorted on their start time.
+	e.g.
+	Input: arr1=[[1, 3], [5, 6], [7, 9]], arr2=[[2, 3], [5, 7]]
+	Output: [2, 3], [5, 6], [7, 7]
+	Explanation: The output list contains the common intervals between the two lists.
+*/
+func intervalIntersection(intervalsA [][]int, intervalsB [][]int) [][]int {
+	return nil
+}
+
+func main() {
+	intervalIntersection([][]int{{1, 3}, {5, 6}, {7, 9}}, [][]int{{2, 3}, {5, 7}})
+}
