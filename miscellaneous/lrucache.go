c50067815e1c630258dea8c0228db341414998d1
Z <mohistzh@gmail.com>
2020-04-26 17:44:25 +0800

    add miscellaneous part, starting from LRUCache

---
+++ miscellaneous/lrucache.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/cheekybits/genny/generic"
+)
+
+// Key generic key
+type Key generic.Type
+
+// Value generic value
+type Value generic.Type
+
+// LRUCache structure
+type LRUCache struct {
+}
+
+// Constructor init structure's capacity
+func Constructor(capacity int) LRUCache {
+
+}
+
+// Get get
+func (lruCache *LRUCache) Get(key generic.Type) generic.Type {
+
+}
+
+// Put add new data
+func (lruCache *LRUCache) Put(key generic.Type, value generic.Type) {
+
+}
