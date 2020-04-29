d50d442704487e40dabac748eafa4160fd446e73
Z <mohistzh@gmail.com>
2020-04-29 11:30:32 +0800

    mi: interval intersection problem;

--- mergeintervals/mi_interval_intersection.go
+++ mergeintervals/mi_interval_intersection.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 /*
 	Given two lists of intervals, find the intersection of these two lists.
 	Each list consists of disjoint intervals sorted on their start time.
@@ -9,9 +14,25 @@ package main
 	Explanation: The output list contains the common intervals between the two lists.
 */
 func intervalIntersection(intervalsA [][]int, intervalsB [][]int) [][]int {
-	return nil
+	// c.start = max(a.start, b.start) c.end = min(a.end, b.end) if c.end < c.start then skip
+	var intervalsC [][]int
+	for i := 0; i < len(intervalsA); i++ {
+		subArrayA := intervalsA[i]
+		for j := 0; j < len(intervalsB); j++ {
+			subArrayB := intervalsB[j]
+			start := int(math.Max(float64(subArrayA[0]), float64(subArrayB[0])))
+			end := int(math.Min(float64(subArrayA[1]), float64(subArrayB[1])))
+			if end < start {
+				continue
+			} else {
+				intervalsC = append(intervalsC, []int{start, end})
+			}
+		}
+	}
+	return intervalsC
 }
 
 func main() {
-	intervalIntersection([][]int{{1, 3}, {5, 6}, {7, 9}}, [][]int{{2, 3}, {5, 7}})
+	fmt.Println(intervalIntersection([][]int{{1, 3}, {5, 6}, {7, 9}}, [][]int{{2, 3}, {5, 7}}))
+	fmt.Println(intervalIntersection([][]int{{1, 3}, {5, 7}, {9, 12}}, [][]int{{5, 10}}))
 }
