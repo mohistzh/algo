ee1feca89be20a8dc2acc8d072082abe147aa286
Z <mohistzh@gmail.com>
2020-05-06 19:59:09 +0800

    sorting cpuload dataset

--- mergeintervals/mi_maximum_cpu_load.go
+++ mergeintervals/mi_maximum_cpu_load.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 // CPULoad data structure
@@ -17,10 +18,21 @@ Our goal is to find the maximum CPU load at any time if all the jobs are running
 */
 func findMaximumCPULoad(jobs []CPULoad) int {
 	fmt.Println(jobs)
+	sortJobs(jobs)
+	fmt.Println(jobs)
 	return -1
+}
+func sortJobs(jobs []CPULoad) {
+	sort.Slice(jobs[:], func(i, j int) bool {
+		return jobs[i].start < jobs[j].start
+	})
 }
 
 func main() {
-	jobs := []CPULoad{CPULoad{start: 1, end: 4, load: 3}, CPULoad{start: 2, end: 5, load: 4}, CPULoad{start: 7, end: 9, load: 6}}
-	fmt.Println(findMaximumCPULoad(jobs))
+	jobs1 := []CPULoad{CPULoad{start: 1, end: 4, load: 3}, CPULoad{start: 2, end: 5, load: 4}, CPULoad{start: 7, end: 9, load: 6}}
+	jobs2 := []CPULoad{CPULoad{start: 6, end: 7, load: 10}, CPULoad{start: 2, end: 4, load: 11}, CPULoad{start: 8, end: 12, load: 15}}
+	jobs3 := []CPULoad{CPULoad{start: 1, end: 4, load: 2}, CPULoad{start: 2, end: 4, load: 1}, CPULoad{start: 3, end: 6, load: 5}}
+	fmt.Println(findMaximumCPULoad(jobs1))
+	fmt.Println(findMaximumCPULoad(jobs2))
+	fmt.Println(findMaximumCPULoad(jobs3))
 }
