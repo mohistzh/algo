a7ecbfb9f5e79a1c6e747a76c90d4ea354bfe871
Z <mohistzh@gmail.com>
2020-05-07 12:03:55 +0800

    find out the intervals of free time

--- mergeintervals/mi_employee_free_time.go
+++ mergeintervals/mi_employee_free_time.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
@@ -11,10 +12,20 @@ For ‘K’ employees, we are given a list of intervals representing the working
 Our goal is to find out if there is a free interval that is common to all employees.
 You can assume that each list of employee working hours is sorted on the start time.
 */
-func findEmployeeFreeTime(schedule [][]int) [][]int {
-	sortMatrixBySlice(schedule) // sorting matrix by first element of array in matrix
-	fmt.Println(schedule)
-	return nil
+func findEmployeeFreeTime(employees [][]int) [][]int {
+	sortMatrixBySlice(employees) // sorting matrix by first element of array in matrix
+	prevMaxEnd := math.MaxInt32
+	var result [][]int
+	const start, end = 0, 1
+	employee := employees[start]
+	for i := 1; i < len(employees); i++ {
+		if prevMaxEnd < employees[i][start] { // comparing previous maximum end with current start
+			result = append(result, []int{prevMaxEnd, employees[i][start]})
+		}
+		prevMaxEnd = int(math.Max(float64(employee[end]), float64(employees[i][end])))
+		employee = employees[i]
+	}
+	return result
 }
 
 // sort 2d array by slice
@@ -31,7 +42,7 @@ func sortMatrixBySlice(matrix [][]int) {
 }
 
 func main() {
-	findEmployeeFreeTime([][]int{{1, 3}, {5, 6}, {2, 3}, {6, 8}})
-	findEmployeeFreeTime([][]int{{1, 3}, {9, 12}, {2, 4}, {6, 8}})
-	findEmployeeFreeTime([][]int{{1, 3}, {2, 4}, {3, 5}, {7, 9}})
+	fmt.Println(findEmployeeFreeTime([][]int{{1, 3}, {5, 6}, {2, 3}, {6, 8}}))
+	fmt.Println(findEmployeeFreeTime([][]int{{1, 3}, {9, 12}, {2, 4}, {6, 8}}))
+	fmt.Println(findEmployeeFreeTime([][]int{{1, 3}, {2, 4}, {3, 5}, {7, 9}}))
 }
