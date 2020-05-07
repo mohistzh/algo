bda592b8dc7c0def4db000df44a6eadb0fc5f919
Z <mohistzh@gmail.com>
2020-05-07 10:31:12 +0800

    sorting matrix

--- mergeintervals/mi_employee_free_time.go
+++ mergeintervals/mi_employee_free_time.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 // findEmployeeFreeTime
 /*
 For ‘K’ employees, we are given a list of intervals representing the working hours of each employee.
@@ -7,7 +12,22 @@ Our goal is to find out if there is a free interval that is common to all employ
 You can assume that each list of employee working hours is sorted on the start time.
 */
 func findEmployeeFreeTime(schedule [][]int) [][]int {
+	sortMatrixBySlice(schedule) // sorting matrix by first element of array in matrix
+	fmt.Println(schedule)
 	return nil
+}
+
+// sort 2d array by slice
+func sortMatrixBySlice(matrix [][]int) {
+	sort.Slice(matrix[:], func(i, j int) bool {
+		for x := range matrix[i] {
+			if matrix[i][x] == matrix[j][x] {
+				continue
+			}
+			return matrix[i][x] < matrix[j][x]
+		}
+		return false
+	})
 }
 
 func main() {
