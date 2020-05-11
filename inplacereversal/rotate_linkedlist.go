927c1fc485616a80888f342f1e5782c2abd20c8b
Z <mohistzh@gmail.com>
2020-05-11 17:29:02 +0800

    rotate linked list

--- inplacereversal/rotate_linkedlist.go
+++ inplacereversal/rotate_linkedlist.go
@@ -12,6 +12,23 @@ type MyNode4 struct {
 Given the head of a Singly LinkedList and a number ‘k’, rotate the LinkedList to the right by ‘k’ nodes.
 */
 func rotateLinkedList(head *MyNode4, k int) *MyNode4 {
+	lastNode := head
+	listLength := 1
+	// count length of the linked list and get the last node
+	for lastNode.next != nil {
+		lastNode = lastNode.next
+		listLength++
+	}
+	lastNode.next = head         // make a circular linked list
+	k %= listLength              // in case the k is great than the list size, we don't need to rotate all length
+	skipLength := listLength - k //
+	lastNodeOfRotatedList := head
+	for i := 1; i < skipLength; i++ {
+		lastNodeOfRotatedList = lastNodeOfRotatedList.next
+	}
+	// points to the sub-list of 'k' endoing nodes
+	head = lastNodeOfRotatedList.next
+	lastNodeOfRotatedList.next = nil
 	return head
 
 }
@@ -31,7 +48,6 @@ func main() {
 	head.next.next = &MyNode4{value: 3}
 	head.next.next.next = &MyNode4{value: 4}
 	head.next.next.next.next = &MyNode4{value: 5}
-	head.next.next.next.next.next = &MyNode4{value: 6}
-	head = rotateLinkedList(head, 3)
+	head = rotateLinkedList(head, 8)
 	printMyNode4(head)
 }
