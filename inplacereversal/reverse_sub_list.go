704712898e0360bcb81f8e99c9f07662a362fba5
Z <mohistzh@gmail.com>
2020-05-11 11:19:50 +0800

    use three parts of linked list solution to solve reverse sub list problem

--- inplacereversal/reverse_sub_list.go
+++ inplacereversal/reverse_sub_list.go
@@ -14,23 +14,41 @@ type MyNode1 struct {
 Given the head of a LinkedList and two positions ‘p’ and ‘q’, reverse the LinkedList from position ‘p’ to ‘q’.
 */
 func reverseSubList(head *MyNode1, pos []int) *MyNode1 {
-	start, end, flag := pos[0], pos[1], false
+	start, end := pos[0], pos[1]
+	if start == end {
+		return head
+	}
 	current := head
 	var prev *MyNode1
-	for current != nil {
-		if current.value == start {
-			flag = true
-		}
-		if flag {
-			//next := current.next
-			current.next = prev
-			prev = current
-			//current = next
-
-		}
-		flag = !(flag && current.value == end)
+	i := 0
+	// first part of the linked list, skipping start - 1 nodes, current will point to 'start'th node
+	for current != nil && i < start-1 {
+		prev = current
 		current = current.next
+		i++
 	}
+	// we are consider three parts of the linked list
+	// the first part is before index 'start', the second part is between 'start' and 'end', the last part after index 'end'
+	lastNodeOfFirstPart := prev
+	lastNodeOfSubList := current
+	i = 0
+	// sub-list part of the linked list
+	for current != nil && i < end-start+1 {
+		next := current.next
+		current.next = prev
+		prev = current // prev is the last node of this part
+		current = next
+		i++
+	}
+	// connect with the first part
+	if lastNodeOfFirstPart != nil {
+		lastNodeOfFirstPart.next = prev
+	} else {
+		head = prev // start from the first node
+	}
+	// connect with the last part
+	lastNodeOfSubList.next = current
+
 	return head
 
 }
