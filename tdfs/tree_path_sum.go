8beb2f255abc8fe74d232564f912fe94cce8944a
Z <mohistzh@gmail.com>
2020-05-16 17:49:52 +0800

    traversal solution with stack

--- tdfs/tree_path_sum.go
+++ tdfs/tree_path_sum.go
@@ -24,6 +24,28 @@ func findBinaryTreePathWithSum(root *MyTreeNode1, sum int) bool {
 	return findBinaryTreePathWithSum(root.left, sum-root.value) || findBinaryTreePathWithSum(root.right, sum-root.value)
 }
 
+// traversal solution with stack TODO
+func findBinaryTreePathWithSum2(root *MyTreeNode1, sum int) bool {
+	if root == nil {
+		return false
+	}
+	var stack []*MyTreeNode1
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		fmt.Print(node.value, " ")
+		if node.right != nil {
+			stack = append(stack, node.right)
+
+		}
+		if node.left != nil {
+			stack = append(stack, node.left)
+		}
+	}
+	return false
+}
+
 func main() {
 	root := &MyTreeNode1{value: 12}
 	root.left = &MyTreeNode1{value: 7}
@@ -34,4 +56,6 @@ func main() {
 
 	fmt.Println(findBinaryTreePathWithSum(root, 23))
 	fmt.Println(findBinaryTreePathWithSum(root, 16))
+
+	fmt.Println(findBinaryTreePathWithSum2(root, 23))
 }
