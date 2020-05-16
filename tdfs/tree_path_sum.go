e52c79bf8bd2bf0d7ebf2e90e9702289dcc2e9bc
Z <mohistzh@gmail.com>
2020-05-16 17:19:44 +0800

    find tree path with sum in recursion solution

--- tdfs/tree_path_sum.go
+++ tdfs/tree_path_sum.go
@@ -14,19 +14,24 @@ type MyTreeNode1 struct {
 Given a binary tree and a number ‘S’, find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
 
 */
-func findBinaryTreePathWithSum(root *MyTreeNode1, sum int) []int {
-	var result []int
-	return result
+func findBinaryTreePathWithSum(root *MyTreeNode1, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.value == sum && root.left == nil && root.right == nil {
+		return true
+	}
+	return findBinaryTreePathWithSum(root.left, sum-root.value) || findBinaryTreePathWithSum(root.right, sum-root.value)
 }
 
 func main() {
-	root := &MyTreeNode1{value: 1}
-	root.left = &MyTreeNode1{value: 2}
-	root.right = &MyTreeNode1{value: 3}
-	root.left.left = &MyTreeNode1{value: 4}
-	root.left.right = &MyTreeNode1{value: 5}
-	root.right.left = &MyTreeNode1{value: 6}
-	root.right.right = &MyTreeNode1{value: 7}
+	root := &MyTreeNode1{value: 12}
+	root.left = &MyTreeNode1{value: 7}
+	root.right = &MyTreeNode1{value: 1}
+	root.left.left = &MyTreeNode1{value: 9}
+	root.right.left = &MyTreeNode1{value: 10}
+	root.right.right = &MyTreeNode1{value: 5}
 
-	fmt.Println(findBinaryTreePathWithSum(root, 10))
+	fmt.Println(findBinaryTreePathWithSum(root, 23))
+	fmt.Println(findBinaryTreePathWithSum(root, 16))
 }
