795e615b644d7cd9eefb569a9bd7e39f259163af
Z <mohistzh@gmail.com>
2020-05-16 16:55:09 +0800

    right views binary tree

--- tbfs/right_view_bt.go
+++ tbfs/right_view_bt.go
@@ -16,7 +16,24 @@ The right view of a binary tree is the set of nodes visible when the tree is see
 */
 func rightViewBinaryTree(root *MyTreeNode10) []int {
 	var result []int
-
+	var queue []*MyTreeNode10
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i == levelSize-1 { // reach to the last node of this level
+				result = append(result, node.value)
+			}
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+	}
 	return result
 }
 
@@ -27,6 +44,6 @@ func main() {
 	root.left.left = &MyTreeNode10{value: 9}
 	root.right.left = &MyTreeNode10{value: 10}
 	root.right.right = &MyTreeNode10{value: 5}
-	root.left.left = &MyTreeNode10{value: 3}
+	root.left.left.left = &MyTreeNode10{value: 3}
 	fmt.Println(rightViewBinaryTree(root))
 }
