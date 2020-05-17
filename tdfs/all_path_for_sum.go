aa19f4e6d59490d4baed20cf68ddaa0253615947
Z <mohistzh@gmail.com>
2020-05-17 20:43:00 +0800

    all path for sum

---
+++ tdfs/all_path_for_sum.go
@@ -0,0 +1,19 @@
+package main
+
+// MyTreeNode2 tree node
+type MyTreeNode2 struct {
+	value       int
+	left, right *MyTreeNode2
+}
+
+/*
+Given a binary tree and a number ‘S’, find all paths from root-to-leaf such that the sum of all the node values of each path equals ‘S’.
+
+*/
+func findAllPathForSum(root *MyTreeNode2, sum int) int {
+	return -1
+}
+
+func main() {
+
+}
