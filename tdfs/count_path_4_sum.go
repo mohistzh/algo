bc13adc273220dcb6a0628917ecb824316e2c0b1
Z <mohistzh@gmail.com>
2020-05-19 15:18:25 +0800

    count paths for a sum

---
+++ tdfs/count_path_4_sum.go
@@ -0,0 +1,17 @@
+package main
+
+// MyTreeNode5 tree node
+type MyTreeNode5 struct {
+	value       int
+	left, right *MyTreeNode5
+}
+
+/*
+Given a binary tree and a number ‘S’, find all paths in the tree such that the sum of all the node values of each path equals ‘S’.
+Please note that the paths can start or end at any node but all paths must follow direction from parent to child (top to bottom).
+
+
+*/
+func countPathsForSum(root *MyTreeNode5, sum int) int {
+	return -1
+}
