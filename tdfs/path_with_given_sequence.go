d184bf617fdb212101c3467f5f53b7d282a3aa0b
Z <mohistzh@gmail.com>
2020-05-19 11:41:17 +0800

    find sequence if present as path in the tree

--- tdfs/path_with_given_sequence.go
+++ tdfs/path_with_given_sequence.go
@@ -14,7 +14,37 @@ type MyTreeNode4 struct {
 Given a binary tree and a number sequence, find if the sequence is present as a root-to-leaf path in the given tree.
 */
 func pathWithGivenSequence(root *MyTreeNode4, sequence []int) bool {
-	return false
+	var currentPath []int
+	return pathWithGivenSequenceRecursive(root, currentPath, sequence)
+}
+func pathWithGivenSequenceRecursive(currentNode *MyTreeNode4, currentPath []int, sequence []int) bool {
+	result := false
+	if currentNode == nil {
+		return result
+	}
+
+	currentPath = append(currentPath, currentNode.value)
+	if currentNode.left == nil && currentNode.right == nil {
+		result = compareSlices(currentPath, sequence)
+		currentPath = currentPath[:len(currentPath)-1]
+		return result
+	}
+	return pathWithGivenSequenceRecursive(currentNode.left, currentPath, sequence) || pathWithGivenSequenceRecursive(currentNode.right, currentPath, sequence)
+
+}
+func compareSlices(a []int, b []int) bool {
+	if a == nil || b == nil {
+		return false
+	}
+	if len(a) != len(b) {
+		return false
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
@@ -23,6 +53,13 @@ func main() {
 	root.right = &MyTreeNode4{value: 9}
 	root.right.left = &MyTreeNode4{value: 2}
 	root.right.right = &MyTreeNode4{value: 9}
-
-	fmt.Println(root, []int{1, 9, 9})
+	fmt.Println(pathWithGivenSequence(root, []int{1, 9, 9}))
+	root = &MyTreeNode4{value: 1}
+	root.left = &MyTreeNode4{value: 0}
+	root.right = &MyTreeNode4{value: 1}
+	root.left.left = &MyTreeNode4{value: 1}
+	root.right.left = &MyTreeNode4{value: 6}
+	root.right.right = &MyTreeNode4{value: 5}
+	fmt.Println(pathWithGivenSequence(root, []int{1, 0, 7}))
+	fmt.Println(pathWithGivenSequence(root, []int{1, 1, 6}))
 }
