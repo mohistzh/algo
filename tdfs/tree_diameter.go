a191e6ee8ba98297e5b62e30c5c917ba3b6897d5
Z <mohistzh@gmail.com>
2020-05-20 15:40:19 +0800

    tree diameter

--- tdfs/tree_diameter.go
+++ tdfs/tree_diameter.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // MyTreeNode6 tree node
 type MyTreeNode6 struct {
 	value       int
@@ -12,20 +17,36 @@ The diameter of a tree may or may not pass through the root.
 
 Note: You can always assume that there are at least two leaf nodes in the given tree.
 */
+var treeDiameter = 0
+
 func findTreeDiameter(root *MyTreeNode6) int {
-	return -1
+	calculateHeight(root)
+	return treeDiameter
+}
+func calculateHeight(currentNode *MyTreeNode6) int {
+	if currentNode == nil {
+		return 0
+	}
+	leftHeight := calculateHeight(currentNode.left)
+	rightHeight := calculateHeight(currentNode.right)
+	diameter := leftHeight + rightHeight + 1
+	treeDiameter = int(math.Max(float64(diameter), float64(treeDiameter)))
+	return int(math.Max(float64(leftHeight), float64(rightHeight))) + 1
 }
 
 func main() {
 	root := &MyTreeNode6{value: 1}
 	root.left = &MyTreeNode6{value: 2}
 	root.right = &MyTreeNode6{value: 3}
+	root.left.left = &MyTreeNode6{value: 4}
 	root.right.left = &MyTreeNode6{value: 5}
 	root.right.right = &MyTreeNode6{value: 6}
-	root.right.right = &MyTreeNode6{value: 9}
-	root.right.right = &MyTreeNode6{value: 11}
+	fmt.Println("Tree diameter:", findTreeDiameter(root))
+	root.left.left = nil
 	root.right.left.left = &MyTreeNode6{value: 7}
 	root.right.left.right = &MyTreeNode6{value: 8}
+	root.right.right = &MyTreeNode6{value: 9}
 	root.right.left.right.right = &MyTreeNode6{value: 10}
-
+	root.right.right = &MyTreeNode6{value: 11}
+	fmt.Println("Tree diameter:", findTreeDiameter(root))
 }
