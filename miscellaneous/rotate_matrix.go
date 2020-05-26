e04ae4ad81feacb1a1d173b7168d32a9dc0f277e
Z <mohistzh@gmail.com>
2020-05-26 17:04:20 +0800

    question defination

--- miscellaneous/rotate_matrix.go
+++ miscellaneous/rotate_matrix.go
@@ -1,12 +1,35 @@
 package main
 
+import (
+	"fmt"
+)
+
 /*
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Given input matrix =
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
 */
 func rotateMatrix(matrix [][]int) {
-
+	fmt.Println(matrix)
 }
 
 func main() {
-
+	matrix := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	rotateMatrix(matrix)
 }
