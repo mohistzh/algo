f505c2ea4ca143129901c2bf99456f277da3a64a
Z <mohistzh@gmail.com>
2020-05-26 16:56:49 +0800

    rotate matrix

--- miscellaneous/plusone.go
+++ miscellaneous/plusone.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 /*
 Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
 
@@ -8,10 +12,31 @@ The digits are stored such that the most significant digit is at the head of the
 You may assume the integer does not contain any leading zero, except the number 0 itself.
 */
 func plusOne(nums []int) []int {
-	return nil
+	if nums == nil || len(nums) < 1 {
+		return nil
+	}
+	index := 0
+	for index = len(nums) - 1; index >= 0; index-- {
+		// e.g. [9, 9]
+		if nums[index] == 9 {
+			nums[index] = 0
+		} else {
+			nums[index]++
+			break
+		}
+	}
+	if index < 0 {
+		result := make([]int, len(nums)+1)
+		result[0] = 1
+		return result
+	} else {
+		return nums
+	}
 }
 
 func main() {
-	plusOne([]int{1, 2, 3})
-	plusOne([]int{4, 3, 2, 1})
+	fmt.Println(plusOne([]int{1, 2, 3}))
+	fmt.Println(plusOne([]int{4, 3, 2, 1}))
+	fmt.Println(plusOne([]int{9, 9}))
+
 }
---
+++ miscellaneous/rotate_matrix.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+*/
+func rotateMatrix(matrix [][]int) {
+
+}
+
+func main() {
+
+}
