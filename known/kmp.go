3ae06b00645fe76595ef50d06d5165f9bc16f213
Z <mohistzh@gmail.com>
2020-05-28 17:53:55 +0800

    start known algorithms

---
+++ known/kmp.go
@@ -0,0 +1,40 @@
+// Knuth-Morris-Pratt algorithm, also known as 'The needle in a haystack'
+package main
+
+/*
+In computer science, the Knuth–Morris–Pratt string-searching algorithm (or KMP algorithm)
+searches for occurrences of a "word" W within a main "text string" S by employing the observation that when a mismatch occurs,
+the word itself embodies sufficient information to determine where the next match could begin, thus bypassing re-examination of previously matched characters.
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+// indexOf S is a text to be searched, W is the word sought, return number of position
+func indexOf(S string, W string) int {
+	if len(S) < 1 {
+		return -1
+	}
+	if len(W) < 1 {
+		return 0
+	}
+	// indexOfS is the position of current character in S
+	// indexOfW is the position of current character in W
+	indexOfS, indexOfW, result := 0, 0, 0
+	for indexOfS < len(S) {
+		if W[indexOfW] == S[indexOfS] {
+			indexOfS++
+			indexOfW++
+			if indexOfW == len(W) {
+
+			}
+		} else {
+
+		}
+	}
+
+	return result
+
+}
+
+func main() {
+
+}
