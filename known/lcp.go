bb06aada74a8708f19c1230551364b53a444f2fa
Z <mohistzh@gmail.com>
2020-05-29 14:57:39 +0800

    longest common prefix

---
+++ known/lcp.go
@@ -0,0 +1,15 @@
+// Longest common prefix
+
+package main
+
+/*
+	Given a set of strings, find the longest common prefix.
+*/
+func lcp(wordlist []string) string {
+	size := len(wordlist)
+	return ""
+}
+
+func main() {
+	lcp([]string{"apple", "ape", "april"})
+}
