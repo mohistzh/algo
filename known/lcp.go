51df9cb0a8d4d49d211f8a5afccb11dde1315934
Z <mohistzh@gmail.com>
2020-05-29 18:01:17 +0800

    longest common prefix

--- known/lcp.go
+++ known/lcp.go
@@ -2,14 +2,38 @@
 
 package main
 
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
 /*
 	Given a set of strings, find the longest common prefix.
 */
 func lcp(wordlist []string) string {
 	size := len(wordlist)
-	return ""
+	// base case
+	if size == 0 {
+		return ""
+	}
+	if size == 1 {
+		return wordlist[0]
+	}
+	//sort the array of strings
+	sort.Strings(wordlist)
+	minimumLength := int(math.Min(float64(len(wordlist[0])), float64(len(wordlist[size-1]))))
+	index := 0
+	// becasue we sorted array first, so it's only need compare with first and last elements
+	for index < minimumLength && wordlist[0][index] == wordlist[size-1][index] {
+		index++
+	}
+	prefix := wordlist[0][0:index]
+	return prefix
 }
 
 func main() {
-	lcp([]string{"apple", "ape", "april"})
+	fmt.Println(lcp([]string{"apple", "ape", "april"}))
+	fmt.Println(lcp([]string{"flower", "flow", "flight"}))
+	fmt.Println(lcp([]string{"dog", "racecar", "car"}))
 }
