65a341866dd86b316fe6c18a4109b81d33f39523
Z <mohistzh@gmail.com>
2020-06-09 18:27:00 +0800

    bottom-top with tabulation

--- dp/subset_sum.go
+++ dp/subset_sum.go
@@ -27,10 +27,43 @@ func hasPartitionRecursive(nums []int, index int, sum int, target int, memo map[
 	memo[current] = hasPartitionRecursive(nums, index+1, sum, target, memo) || hasPartitionRecursive(nums, index+1, sum+nums[index], target, memo)
 	return memo[current]
 }
+
+// HasPartitionTabulation tabulation approach
+func HasPartitionTabulation(nums []int, target int) bool {
+	dp := make([][]bool, len(nums))
+	for i := 0; i < len(nums); i++ {
+		dp[i] = make([]bool, target+1)
+		dp[i][0] = true
+	}
+
+	for i := 1; i <= target; i++ {
+		state := false
+		if nums[0] == i {
+			state = true
+		}
+		dp[0][i] = state
+	}
+
+	for i := 1; i < len(nums); i++ {
+		for s := 1; s <= target; s++ {
+			if dp[i-1][s] {
+				dp[i][s] = dp[i-1][s]
+			} else if s >= nums[i] {
+				dp[i][s] = dp[i-1][s-nums[i]]
+			}
+		}
+	}
+	return dp[len(nums)-1][target]
+
+}
 func main() {
 
 	res := HasPartition([]int{1, 2, 7, 1, 5}, 10)
 	fmt.Println(res)
 	res = HasPartition([]int{1, 3, 4, 8}, 6)
+	fmt.Println(res)
+	res = HasPartitionTabulation([]int{1, 2, 7, 1, 5}, 10)
+	fmt.Println(res)
+	res = HasPartitionTabulation([]int{1, 3, 4, 8}, 6)
 	fmt.Println(res)
 }
