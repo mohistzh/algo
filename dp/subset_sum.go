b29c5eb3f91d2587fe5159a81cba1834ffa702a9
Z <mohistzh@gmail.com>
2020-06-09 17:27:30 +0800

    top-bottom with memoization

--- dp/subset_sum.go
+++ dp/subset_sum.go
@@ -8,17 +8,24 @@ import (
 	Given a set of positive numbers, determine if there exists a subset whose sum is equal to given number 'S'
 */
 func HasPartition(nums []int, target int) bool {
-	return hasPartitionRecursive(nums, 0, 0, target)
+	memo := make(map[string]bool)
+	return hasPartitionRecursive(nums, 0, 0, target, memo)
 }
-func hasPartitionRecursive(nums []int, index int, sum int, target int) bool {
+
+// hasPartitionRecursive Top-bottom with memoization approach
+func hasPartitionRecursive(nums []int, index int, sum int, target int, memo map[string]bool) bool {
+	current := fmt.Sprint(index, "", sum)
+	if _, ok := memo[current]; ok {
+		return memo[current]
+	}
 	if sum == target {
 		return true
 	}
 	if sum > target || index >= len(nums) {
 		return false
 	}
-
-	return hasPartitionRecursive(nums, index+1, sum, target) || hasPartitionRecursive(nums, index+1, sum+nums[index], target)
+	memo[current] = hasPartitionRecursive(nums, index+1, sum, target, memo) || hasPartitionRecursive(nums, index+1, sum+nums[index], target, memo)
+	return memo[current]
 }
 func main() {
 
