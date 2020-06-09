be7f9fe51766f1f4d7a74f914630e67fe2b6af16
Z <mohistzh@gmail.com>
2020-06-09 14:16:25 +0800

    init subset sum problem

---
+++ dp/subset_sum.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+	Given a set of positive numbers, determine if there exists a subset whose sum is equal to given number 'S'
+*/
+func HasPartition(nums []int, sum int) bool {
+	return false
+}
+
+func main() {
+
+}
