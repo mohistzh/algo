ea912123cee43c619c2105b5212f0394d00707bf
Z <mohistzh@gmail.com>
2020-06-10 16:41:48 +0800

    count of subset sum

---
+++ dp/count_subset_sum.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+	Given a set of positive numbers, find the total number of subsets whose sum is equal to a given number 'sum'
+*/
+func CountOfSubsetSum(nums []int, sum int) int {
+
+}
+
+func main() {
+	res := CountOfSubsetSum([]int{1, 1, 2, 3}, 4)
+	fmt.Println(res)
+}
