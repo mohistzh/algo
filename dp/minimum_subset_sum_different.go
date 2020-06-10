36ef232e5363aed61c1fda967e53755f59a52bbb
Z <mohistzh@gmail.com>
2020-06-10 11:36:04 +0800

    memoization approach

--- dp/minimum_subset_sum_different.go
+++ dp/minimum_subset_sum_different.go
@@ -26,6 +26,24 @@ func minimumSubSetSumDifferentRecursive(nums []int, index int, sum1 int, sum2 in
 	return int(math.Min(float64(diff1), float64(diff2)))
 }
 
+// MinimumSubSetSumDifferentMemoization based on naive solution, add a cache mechanism
+func MinimumSubSetSumDifferentMemoization(nums []int) int {
+	memo := make(map[string]int)
+	return minimumSubSetSumDifferentMemoization(nums, 0, 0, 0, memo)
+}
+func minimumSubSetSumDifferentMemoization(nums []int, index int, sum1 int, sum2 int, memo map[string]int) int {
+	current := fmt.Sprint(index, "", sum1)
+	if index == len(nums) {
+		return intAbs(sum1 - sum2)
+	}
+	if _, ok := memo[current]; !ok {
+		diff1 := minimumSubSetSumDifferentMemoization(nums, index+1, sum1+nums[index], sum2, memo)
+		diff2 := minimumSubSetSumDifferentMemoization(nums, index+1, sum1, sum2+nums[index], memo)
+		memo[current] = int(math.Min(float64(diff1), float64(diff2)))
+	}
+	return memo[current]
+}
+
 // lazy to convert data type
 func intAbs(x int) int {
 	if x < 0 {
@@ -38,5 +56,10 @@ func main() {
 	res := MinimumSubSetSumDifferent([]int{1, 2, 3, 9})
 	fmt.Println(res)
 	res = MinimumSubSetSumDifferent([]int{1, 3, 100, 4})
+	fmt.Println(res)
+
+	res = MinimumSubSetSumDifferentMemoization([]int{1, 2, 3, 9})
+	fmt.Println(res)
+	res = MinimumSubSetSumDifferentMemoization([]int{1, 3, 100, 4})
 	fmt.Println(res)
 }
