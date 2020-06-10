a7924a6afa2658b4275bb8a3f0ca72ce6a3511f7
Z <mohistzh@gmail.com>
2020-06-10 10:31:33 +0800

    space complexity improvements

--- dp/subset_sum.go
+++ dp/subset_sum.go
@@ -56,6 +56,37 @@ func HasPartitionTabulation(nums []int, target int) bool {
 	return dp[len(nums)-1][target]
 
 }
+
+// HasPartitionTabulation2 space complexity costs O(target)
+func HasPartitionTabulation2(nums []int, target int) bool {
+	dp := make([][]bool, 2)
+	for i := 0; i < 2; i++ {
+		dp[i] = make([]bool, target+1)
+		dp[i][0] = true
+	}
+
+	for i := 1; i <= target; i++ {
+		state := false
+		if nums[0] == i {
+			state = true
+		}
+		dp[0][i] = state
+	}
+
+	for i := 1; i < len(nums); i++ {
+		for s := 1; s <= target; s++ {
+			if dp[(i-1)%2][s] {
+				dp[i%2][s] = dp[(i-1)%2][s]
+			} else if s >= nums[i] {
+				dp[i%2][s] = dp[(i-1)%2][s-nums[i]]
+			}
+		}
+	}
+
+	return dp[(len(nums)-1)%2][target]
+
+}
+
 func main() {
 
 	res := HasPartition([]int{1, 2, 7, 1, 5}, 10)
@@ -65,5 +96,9 @@ func main() {
 	res = HasPartitionTabulation([]int{1, 2, 7, 1, 5}, 10)
 	fmt.Println(res)
 	res = HasPartitionTabulation([]int{1, 3, 4, 8}, 6)
+	fmt.Println(res)
+	res = HasPartitionTabulation2([]int{1, 3, 4, 8}, 6)
+	fmt.Println(res)
+	res = HasPartitionTabulation2([]int{1, 2, 7, 1, 5}, 10)
 	fmt.Println(res)
 }
