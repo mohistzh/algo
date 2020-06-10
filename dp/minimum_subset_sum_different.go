a213214e9f3be5001aba81236b9b3b24d3821efe
Z <mohistzh@gmail.com>
2020-06-10 10:48:12 +0800

    minimum subset sum different

---
+++ dp/minimum_subset_sum_different.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+	Given a set of positive numbers, partition the set into two subsets with a minimum different between their subset sums
+*/
+func MinimumSubSetSumDifferent(nums []int) int {
+	return 0
+}
+
+func main() {
+	MinimumSubSetSumDifferent([]int{1, 2, 3, 9})
+}
