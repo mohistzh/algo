43a21d8d814ccd8992e7370d974426236a310bed
Z <mohistzh@gmail.com>
2020-07-09 10:19:29 +0800

    set matrix zeroes

---
+++ miscellaneous/matrix_zeroes.go
@@ -0,0 +1,17 @@
+package main
+
+/**
+	Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+**/
+func setMatrixZeroes(matrix [][]int) {
+
+}
+
+func main() {
+	matrix := [][]int{
+		{1, 1, 1},
+		{1, 0, 1},
+		{1, 1, 1},
+	}
+	setMatrixZeroes(matrix)
+}
