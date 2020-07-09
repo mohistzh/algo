50f14000619d6f1d535cdba88b747cc86d51d7e4
Z <mohistzh@gmail.com>
2020-07-09 14:40:52 +0800

    ascii code calculation

--- miscellaneous/group_anagram.go
+++ miscellaneous/group_anagram.go
@@ -33,6 +33,37 @@ func groupAnagrams(input []string) [][]string {
 	return result
 }
 
+/*
+	use ascii code calculation instead of sort string
+*/
+func groupAnagrams2(input []string) [][]string {
+	auxMap := make(map[string][]string)
+	var result [][]string
+	for i := 0; i < len(input); i++ {
+		str := input[i]
+		// lower case
+		letters := make([]byte, 26)
+		for j := 0; j < len(str); j++ {
+			letters[str[j]-'a']++
+		}
+
+		key := string(letters)
+
+		if val, ok := auxMap[key]; ok {
+			val = append(val, str)
+			auxMap[key] = val
+		} else {
+			auxMap[key] = []string{str}
+		}
+
+	}
+	for _, v := range auxMap {
+		result = append(result, v)
+	}
+	return result
+
+}
+
 func sortString(str string) string {
 	arr := strings.Split(str, "")
 	sort.Strings(arr)
@@ -50,4 +81,6 @@ func main() {
 		"eat", "tea", "tan", "ate", "nat", "bat",
 	}
 	fmt.Println(groupAnagrams(sample1))
+	fmt.Println()
+	fmt.Println(groupAnagrams2(sample1))
 }
