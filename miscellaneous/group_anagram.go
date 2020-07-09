3ee496cdd95b5483373b0a5d01406f712d9b8e5a
Z <mohistzh@gmail.com>
2020-07-09 13:42:33 +0800

    group anagrams

---
+++ miscellaneous/group_anagram.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/*
+	Given an array of strings, group anagrams together.
+
+	Note:
+	1. All inputs will be in lowercase.
+	2. The order of your output does not matter.
+*/
+func groupAnagrams(input []string) [][]string {
+	var result [][]string
+
+	return result
+}
+
+func main() {
+	sample1 := []string{
+		"eat", "tea", "tan", "ate", "nat", "bat",
+	}
+	fmt.Println(groupAnagrams(sample1))
+}
